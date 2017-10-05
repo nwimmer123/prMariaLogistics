3f2b22b830272f136e1c3034595512809643cd50
Noah Wimmer <nwimmer123@yahoo.com>
2017-10-05 15:09:29 -0700

    moved root and set up splash

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "sessions#new"
+  root to: "users#index"
   
     get "/users", to: "users#index", as: "users"
     get "/users/new", to: "users#new", as: "new_user"
