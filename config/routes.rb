62949bdbe873617b8a0763742efac086896f0d8a
Noah Wimmer <nwimmer123@yahoo.com>
2017-10-05 12:47:21 -0700

    simplified

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: "sessions#new"
-  
-    get "/users", to: "users#index", as: "users"
-    get "/users/new", to: "users#new", as: "new_user"
-    post "/users", to: "users#create"
-    get "/users/:id", to: "users#show", as: "user"
-    get "/users/:id/edit", to: "users#edit", as: "edit_user"
-    patch "/users/:id", to: "users#update"
-    delete "/users/:id", to: "users#destroy", as: "delete_user"
+  root to: "assistance_types#index"
+
+    resources :users
+    resources :assistance_types
   
     get "/login", to: "sessions#new", as: "login"
     get "/logout", to: "sessions#destroy"
