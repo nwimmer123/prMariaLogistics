afb2658ae0bbb70fafde9bbe5d9dda3fc940a343
Noah Wimmer <nwimmer123@yahoo.com>
2017-10-05 15:48:45 -0700

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
 
   root to: "users#index"
-  
-    get "/users", to: "users#index", as: "users"
-    get "/users/new", to: "users#new", as: "new_user"
-    post "/users", to: "users#create"
-    get "/users/:id", to: "users#show", as: "user"
-    get "/users/:id/edit", to: "users#edit", as: "edit_user"
-    patch "/users/:id", to: "users#update"
-    delete "/users/:id", to: "users#destroy", as: "delete_user"
+
+    resources :users
+    resources :cargos
   
     get "/login", to: "sessions#new", as: "login"
     get "/logout", to: "sessions#destroy"
