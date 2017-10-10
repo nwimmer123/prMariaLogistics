1c68d5e212e3ff3f511dff6f175730b0fe066b24
Noah Wimmer <nwimmer123@yahoo.com>
2017-10-09 17:16:41 -0700

    generated

---
+++ db/migrate/20171010001616_create_transports.rb
@@ -0,0 +1,15 @@
+class CreateTransports < ActiveRecord::Migration[5.1]
+  def change
+    create_table :transports do |t|
+      t.string :role
+      t.string :range
+      t.string :capacity
+      t.string :name
+      t.string :avaliability
+      t.string :price
+      t.string :hub
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009230044) do
+ActiveRecord::Schema.define(version: 20171010001616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 20171009230044) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_cargos_on_user_id"
+  end
+
+  create_table "transports", force: :cascade do |t|
+    t.string "role"
+    t.string "range"
+    t.string "capacity"
+    t.string "name"
+    t.string "avaliability"
+    t.string "price"
+    t.string "hub"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
