6b7acdec1184f27f857062bce970e133c3b19eaa
Noah Wimmer <nwimmer123@yahoo.com>
2017-10-13 10:25:23 -0700

    basic controller methods

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -1,2 +1,46 @@
 class WorkflowsController < ApplicationController
+  before_action :current_workflow, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:create, :show, :update, :destroy]
+
+  def index
+    @workflows = Workflow.all
+  end
+
+  def new
+    @workflow = Workflow.new
+  end
+
+  def create
+    @workflow = Workflow.new(workflow_params)
+    @workflow.user = current_user
+    @workflow.save
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @workflow.update_attributes(workflow_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def destroy
+    @workflow.destroy
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def workflow_params
+    params.require(:workflow).permit(:one, :two, :three, :four, :five, :six, :seven, :eight, :nine, :ten, :elen, :twelve, :thirteeen, :fourteen, :fifteen, :step)
+  end
+
+  def current_workflow
+    @workflow = Workflow.find_by_id(params[:id])
+  end
+  
 end
